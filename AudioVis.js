feae1980751523a93f357b1959b0cfb7c023d42c
Carlo Ruggiero <60533469+crugggiero@users.noreply.github.com>
2021-08-06 14:45:26 -0700

    center button

--- AudioVis.js
+++ AudioVis.js
@@ -1,21 +1,34 @@
 var song;
 var fft;
+var button;
 var freq = 256;
 var volume = 1;
-
-var song1 = 'music/Raw Raw Ft. Lil Uzi Vert - Travis Scott (320kbps).mp3';
-var song2 = 'music/trilla - A$AP Rocky.mp3';
-var song3 = 'music/Travis Scott - Backyard.mp3';
-var song4 = 'music/Pretty Lights - Finally Moving.mp3';
+var opacity = 256;
+var name = 'music/Pretty Lights - Finally Moving.mp3';
 
 
-function preload() {  }
+function preload() { 
+	song = loadSound(name); 
+	button = loadImage('images/button.png');
+}
 
 function setup() {
-	createCanvas(1500, 710);
+	createCanvas(windowWidth - 20, windowHeight - 10);
 	background(0);
 	fft = new p5.FFT(0.6, freq);
-	song = loadSound(song4);
+}
+
+function grid() {
+	push();
+	translate(width/2, height/2);
+
+	strokeWeight(.5);
+	noFill();
+	stroke(255, 0, 0);
+	line(-1000, 0, 1000, 0);
+	line(0, -500, 0, 500);
+	pop();
+
 }
 
 function draw() {
@@ -23,9 +36,23 @@ function draw() {
 	background(0);
 	var spectrum = fft.analyze();
 	var amp;
+	var dx = 0;
 
+	if(song.isPlaying())
+		dx = -16;
+	else
+		dx = 16;
+	
+	if(opacity > 0 && dx < 0)
+		opacity += dx;
+	if(opacity <= 256 && dx > 0)
+		opacity += dx;
+
+	playButton(opacity);
 
 	//bass tones in background
+
+	noStroke();
 
 	for(let i = 0; i < 10; i++) {
 		amp = spectrum[i+10];
@@ -35,16 +62,15 @@ function draw() {
 			let level = amp / 10;
 			
 			for(let j = 0; j < level; j++) {
-				let x = 10 + (150 * i);
+				let x = (width*.096) * i;
 				let y = height - (j * 100) + 5;
 				strokeWeight(1);
 				stroke(1);
 				fill(221 - i*15, 0 + i*15, 255, 40);
-				rect(x, y, 130, 90, 10);
+				rect(x + width/33, y, width*.087, 90, 10);
 			}
 		}
 	}
-
 
 	//center circle
 
@@ -52,6 +78,7 @@ function draw() {
 	translate(width/2, height/2);
 	rotate(-PI/2);
 	noFill();
+	strokeWeight(3);
 
 	for(let i = 15; i < 325; i++) {
 
@@ -68,12 +95,21 @@ function draw() {
 			amp = spectrum[340 - i];
 
 		var r = map(amp, 0, 256, 200, 400);
-		stroke(221 - i/1.3, 0 + i/1.3, 255);
-		strokeWeight(3);
+		stroke(221 - i/1.3, i/1.3, 255);
 		arc(0, 0, r, r, ((2*PI)/310)*(i-15), ((2*PI)/310)*(i-14));
 	}
+
 	pop();
-	
+}
+
+function playButton(opacity) {
+
+	push();
+	translate(width/2 - 47, height/2 - 50);
+	scale(.2, .2);
+	tint(opacity);
+	image(button, 0, 0);
+	pop();
 
 }
 
@@ -118,13 +154,11 @@ function toggle() {
 	}
 }
 
-function impCircle(x,y,cx,cy,r)
-{
+function impCircle(x,y,cx,cy,r) {
 	return sq(x-cx)+sq(y-cy)-sq(r);
 }
 
-function impRect(x,y,x1,y1,dx,dy)
-{
+function impRect(x,y,x1,y1,dx,dy) {
 	if(x<(x1+dx) && x>x1 && y > y1 && y<(y1+dy))
 		return -1;
 	else
